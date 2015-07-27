65c3be005cad859dfc27643fe8c0eb2c849316d1
Your Name <joseph_m_morgan@yahoo.com>
2015-07-27 14:59:00 -0400

    Adding first pass at home page view and submission form.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,18 @@
 class WelcomeController < ApplicationController
 
   def index
-    render text: 'Welcome'
+    if (input = params[:input])
+      output = "That's a good question"
+      session[:inputs] ||= []
+      session[:outputs] ||= []
+      session[:inputs] << input
+      session[:outputs] << output
+    end
+  end
+
+  def reset
+    session[:inputs] = []
+    session[:outputs] = []
+    redirect_to :root
   end
 end
